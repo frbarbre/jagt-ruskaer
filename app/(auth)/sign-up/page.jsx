417f6402d4bae38e56308a7c6e9cf29b148a14fd
frbarbre <fr.barbre@gmail.com>
2023-12-15 19:17:57 +0100

    fixed typo

--- app/(auth)/sign-up/page.jsx
+++ app/(auth)/sign-up/page.jsx
@@ -179,7 +179,7 @@ export default function SignUp() {
               ) : (
                 <SmilePlus className="mr-2 h-4 w-4" />
               )}
-              <span>Login</span>
+              <span>Bliv medlem</span>
             </Button>
             <Or />
             <Link href={"/login"}>
